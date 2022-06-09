e60a759b3f1baaa969e3510ebe0515d96d17856c
RiteshRijal <92074675+RiteshRijal@users.noreply.github.com>
2022-06-09 23:07:04 +0545

    new
    tme haru lai man parena vhane change gar

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="main1.css">
+
+
+</head>
+<body>
+
+
+
+
+  <form >   
+<h2>Marks Leaks</h2>
+      
+          
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Enter your name" required>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+      
+  </form>
+
+
+</body>
+</html>
