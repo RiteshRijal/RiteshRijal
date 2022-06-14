4f9502057341014482c7fa373906d516d9b1e07a
RiteshRijal <92074675+RiteshRijal@users.noreply.github.com>
2022-06-14 14:23:01 +0545

    Add files via upload

---
+++ mul.php
@@ -0,0 +1,72 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+    
+</head>
+<body>
+  <div class="row">
+    <div class="col-4">
+      MARKS Chat
+    </div>
+  
+<div class="topnav">
+  <input type="text" placeholder="Search.." name="search2">
+  <button >GO</button><br>
+
+  <a href="#home">Home</a>
+  <a href="#news">Partners</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">News</a>
+</div>
+</div>
+<div class="header">
+</div>
+<div class="section">
+  <div class="col-3">
+    <img src="../img/download.jpg">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+<textarea name="message" placeholder="enter your opinion">
+</textarea>
+<button type="submit">Send</button>
+</div>
+<div class="col-3">
+  <img src="../img/download.jpg">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+<textarea name="message" placeholder="Enter your opinion" >
+</textarea> 
+<button>Send</button>
+</div>
+<div class="col-3">
+  <img src="../img/download.jpg">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+<textarea name="message" placeholder="Enter your opinion" >
+</textarea> 
+<button>Send</button>
+</div>
+</div>
+
+
+</body>
+</html>
